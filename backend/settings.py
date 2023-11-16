3b1651b626ebdc600f1f9fe3a11df465acb6381d
BugBrekr <bugbrekr@bugbrekr.dev>
2023-11-16 19:08:21 +0530

    fixed cors error

--- backend/settings.py
+++ backend/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-     'http://127.0.0.1:3000'
+     'http://127.0.0.1:3000',
+     'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'backend.urls'
